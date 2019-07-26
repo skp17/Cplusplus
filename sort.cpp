9478402a47452c218a3d1b43122ad4143f51bc51
skp17 <steven_peters07@hotmail.com>
2019-07-26 01:56:50 -0400

    Updated sort.cpp

--- sort.cpp
+++ sort.cpp
@@ -7,23 +7,34 @@ int main() {
 
 double m_array[5] = {36, 21, 5, 81, 11};
 
-long double min;
-long double temp;
+double min;
+double temp;
+int j, position;
 
 for(int i = 0; i < 5; i++) {
 	
-	min = &m_array[i];
+	min = m_array[i];
 
-	for(int j = i + 1; j < 5; j++) {
+	for(j = i + 1; j < 5; j++) {
 		
-		if(m_array[j] < min)
-			min = &m_array[j];
+		if(m_array[j] < min) {
+			min = m_array[j];
+			position = j;
+		}
 	}
 
-	temp = &m_array[i];
-	&m_array[i] = min;
+	temp = m_array[i];
+	m_array[i] = m_array[position];
+	m_array[position] = temp;
 
 }
+
+cout << "The sorted array is ";
+
+for(int i = 0; i < 5; i++)
+  cout << m_array[i] << " ";
+
+cout << endl; 
 
 return 0;
 }
