d2d5c7f86c6cea2bff6ab9ea7400373cce6f54d3
Steve Peters <steven_peters@hotmail.com>
2019-08-02 19:40:52 -0400

    Improved readability of sort programs

--- sort_asc.cpp
+++ sort_asc.cpp
@@ -8,34 +8,23 @@
 
 using namespace std;
 
+void swap_elements(double &x, double &y) {
+	double temp = x;
+	x = y;
+	y = temp;
+}
+
+
 int main() {
 
 int size = 10;
 double m_array[size] = {12, 234, 23, 1, -7, 55, 18, 67, 99, 100};
 
-double min;
-double temp;
-int position;
-bool found; // True if a smaller number is found. Or else false.
-
 for(int i = 0; i < size; i++) {
-	
-	min = m_array[i];
-	found = false;
-
 	for(int j = i + 1; j < size; j++) {
 		
-		if(m_array[j] < min) {
-			min = m_array[j];
-			position = j;
-			found = true;
-		}
-	}
-
-	if(found) {
-		temp = m_array[i];
-		m_array[i] = m_array[position];
-		m_array[position] = temp;
+		if(m_array[j] < m_array[i])
+			swap_elements(m_array[i], m_array[j]);
 	}
 }
 
