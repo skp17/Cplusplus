47ed298b6b11b6f04af62bae3c93e7516e575fa7
Steven Peters <steven_peters07@hotmail.com>
2019-08-23 15:46:33 -0400

    using getopt_long

--- sort_v1.cpp
+++ sort_v1.cpp
@@ -1,6 +1,4 @@
-/* 
- * sort_v1.cpp
- *
+/* sort_v1.cpp
  * Author: Steven Peters			
  * Date: August 14, 2019
  * 	
@@ -13,14 +11,15 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <stdlib.h>
-#include <unistd.h>
+#include <stdlib.h>			/* for exit */
+#include <getopt.h>
+#include <iomanip>			/* setprecision */
 #include <unordered_set>
-#include <algorithm>
+#include <algorithm>		/* for sort */
 
 using namespace std;
 
-void print_usage(char prgm_nm[]);
+void print_usage();
 
 void remove_duplicates(vector<double> &vect);
 
@@ -29,20 +28,40 @@ void read_from_file(vector<double> &vect, string &f_name);
 void write_to_file(vector<double> &vect, string &f_name);
 
 const string DEFAULT_FILENAME = "sorted.dat"; // Default output name
+string program_name;
 
 int main(int argc, char *argv[]) {
 
-	extern char *optarg;
-	extern int optind;
+	const float PRGM_VER = 3.0; 	// Program version
+	program_name = argv[0];
+	string input_name, output_name = DEFAULT_FILENAME;
+	vector<double> v;	/* This vector contains all numbers from the input file */
+
+	// aflag is ascending; dflag is descending; rflag is remove duplicates
+	// iflag is input 
+	int aflag = 0, dflag = 0, rflag = 0, iflag = 0;
 	int c, err = 0;
 
-	// aflag is ascending; dflag is descending; iflag is input 
-	int aflag = 0, dflag = 0, iflag = 0;
-	string input_name, output_name = DEFAULT_FILENAME;
+	while(1) {
+		int option_index = 0;
+		static struct option long_options [] = {
+			{"ascending",		no_argument,		&aflag,	1},
+			{"descending",		no_argument,		&dflag,	1},
+			{"rm_duplicates",	no_argument,		&rflag, 1},
+			{"input",			required_argument,	&iflag,	1},	
+			{"help",			no_argument,			0,	'h'},
+			{"version",			no_argument,			0,	'v'},
+			{0,					0,						0,	0 },
+		};
+		
+		c = getopt_long(argc, argv, "adri:hv", long_options, &option_index);
 
-	vector<double> v; // This vector contains all numbers from the input file
+		/* Detect the end of the options. */
+		if(c == -1)
+			break;
 
-	while((c = getopt(argc, argv, "adi:h")) != -1) {
+		cout << setprecision(1) << fixed;
+
 		switch(c) {
 			case 'a':
 				aflag = 1;
@@ -50,35 +69,43 @@ int main(int argc, char *argv[]) {
 			case 'd':
 				dflag = 1;
 				break;
+			case 'r':
+				rflag = 1;
+				break;
 			case 'i':
 				iflag = 1;
 				input_name = optarg;
 				break;
 			case 'h':
-				print_usage(argv[0]);
+				print_usage();
+				exit(1);
+			case 'v':
+				cout << program_name << " " << PRGM_VER << endl;
 				exit(1);
 			case '?':
+				if (optopt == 'i')
+					iflag = 1;
 				err = 1;
 				break;
+			default:
+				abort();
 		}
 	}
 
+	// Handle errors
+	if( aflag == 1 && dflag == 1 ) {
+		cerr << "Choose either -a or -d. Not both\n";
+		exit(1);
+	}
 	if(iflag == 0) {	/* -i is manditory */
 		cerr << argv[0] << ": missing -i option\n" << endl;
-		print_usage(argv[0]);
-		exit(1);
-	} else if((optind) > argc) {
-		/* need at least one argument for input file */
-		
-		cerr << argv[0] << ": missing input filename" << endl;
-		print_usage(argv[0]);
+		print_usage();
 		exit(1);
 	} else if(err) {
-		print_usage(argv[0]);
+		print_usage();
 		exit(1);
 	}
 
-	
 	// Read elements from file
 	read_from_file(v, input_name);
 
@@ -87,6 +114,8 @@ int main(int argc, char *argv[]) {
 		sort(v.begin(), v.end());
 	if(dflag)
 		sort(v.begin(), v.end(), greater<double>());
+	if(rflag)
+		remove_duplicates(v);
 
 	if (optind < argc)	/* Takes output file take from command line if present */
 			output_name = argv[optind];
@@ -97,13 +126,14 @@ int main(int argc, char *argv[]) {
 	return 0;
 }
 
-void print_usage(char prgm_nm[]) {
-	cerr << "usage: " << prgm_nm << " [-ad] -i input [output]\n\n";
-	cerr << "\t-h \tdisplay this message\n";
+void print_usage() {
+	cerr << "usage: " << program_name << " [-a|d] [r] -i <infile> [<outfile>]\n\n";
+	cerr << "  outfile: \toutput file [default = " << DEFAULT_FILENAME << "]\n\n";
 	cerr << "\t-a \tascending order\n";
 	cerr << "\t-d \tdescending order\n";
-	cerr << "\toutput \t[default = " << DEFAULT_FILENAME << "]\n";
-	//cerr << "-i precedes the input filename\n";
+	cerr << "\t-r \tremove duplicates\n";
+	cerr << "\t-h \tdisplay this message\n";
+	cerr << "\t-v \tdisplay program version\n";
 	}
 
 void remove_duplicates(vector<double> &vect) {
@@ -132,9 +162,12 @@ void read_from_file(vector<double> &vect, string &f_name) {
 			vect.push_back(temp);
 		}
 	}
-	else
+	else {
 		cout << "Error. File could not be opened" << endl;
+		exit(1);
+	}
 
+	// Close input stream
 	in.close();
 }
 
@@ -151,16 +184,17 @@ void write_to_file(vector<double> &vect, string &f_name) {
 			out << vect[i] << " ";
 
 			// File will contain 30 numbers in each row
-			if( ( (i+1) % 30 ) == 0 )
+			if( (i != 0) && (i%30 == 0) )
 				out << endl;
 		}
+		cout << "Sorted numbers written to " << f_name << endl;
 	}
-	else
+	else {
 		cout << "Error. File could not be opened." << endl;
+		exit(1);
+	}
 
 	// Close output stream
 	out.close();
-
-	cout << "Sorted numbers written to " << f_name << endl;
 }
 
